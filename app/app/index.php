d0d4d9ccb9671882a7157fb2522a8bd1fb26a7f2
Piotr Pochanke <ppochanke@gmail.com>
2019-05-25 13:22:34 +0200

    Bootstrap logon

--- app/app/index.php
+++ app/app/index.php
@@ -1,39 +1,157 @@
-<?php
-
-session_start();
-
-if ((isSet($_SESSION['signed_in'])) && ($_SESSION['signed_in'] == true)) {
-    require "check_permissions.php";
-    exit();
-}
-?>
-<!DOCTYPE HTML>
-<html>
-
+<!doctype html>
+<html lang="en">
 <head>
-    <title> Sign in </title>
+	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+	<title>PLED - Planer Egzaminów Dyplomowych</title>
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
 
 </head>
-
-
 <body>
-<div id="container">
-    <div id="sign_in">
-        <form action="sign_in.php" method="post">
-            <b>SIGN IN</b><br/><br/>
-            <b>Username: </b><br/>
-            <input type="text" name="login" value="" size="30"><br/>
-            <b>Password:</b><br/>
-            <input type="password" name="user_password" value="" size="30"><br/>
-            <input type="submit" value="Sign in">
-        </form>
-        <?php
-        if (isSet($_SESSION['error'])) {
-            echo "<p>".$_SESSION['error']."<p/>";
-        }
-        ?>
+
+<div class="wrapper">
+    <div class="sidebar" data-color="green" data-image="assets/img/sidebar-5.jpg">
+
+    <!--
+
+        Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+        Tip 2: you can also add an image using data-image tag
+
+    -->
+
+    	<div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="index.php" class="simple-text">
+                    Planer Egzaminów Dyplomowych
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li class="active">
+                    <a href="#">
+                        <i class="pe-7s-menu"></i>
+                        <p>Strona Główna</p>
+                    </a>
+                </li>
+
+            </ul>
+    	</div>
     </div>
 
+    <div class="main-panel">
+        <nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">PLED - Planer Egzaminów Dyplomowych</a>
+                </div>
+            </div>
+        </nav>
+
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                <div class="col-md-12">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Zaloguj się</h4>
+                            </div>
+                                    <div class ="content">
+                                    <form action="sign_in.php" method="post">
+                                        <div class="row">                                            
+                                             <div class="col-md-3">                                                                         
+                                                <label>Login</label>
+                                                <input type="text" name="login" class="form-control" placeholder="Wpisz..." size="30">
+                                            </div>  
+                                        </div>
+                                        <div class="row">                          
+                                            <div class="col-md-3">
+                                                <label for="password">Hasło</label>
+                                                <input type="password" class="form-control" name="user_password" placeholder="Wpisz..." size="30">
+                                            </div>
+                                        </div>
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <button type="submit" class="btn btn-info btn-fill pull-right">Zaloguj</button>
+                                                </div>
+                                            </div>
+                                            <?php
+                                            if (isSet($_SESSION['error'])) {
+                                                echo "<p>".$_SESSION['error']."<p/>";
+                                            }
+                                            ?>
+                                            
+                                        </div>
+                                        </form>
+                                </div>                                                   
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <footer class="footer">
+            <div class="container-fluid">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+
+                    </ul>
+                </nav>
+                <p class="copyright pull-right">
+                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://www.uz.zgora.pl/">Uniwersytet Zielonogórski</a>
+                </p>
+            </div>
+        </footer>
+
+    </div>
 </div>
+
+
 </body>
+
+    <!--   Core JS Files   -->
+    <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+	<!--  Charts Plugin -->
+	<script src="assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+	<script src="assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
+
+
 </html>
